c48415001ec5153eef95ad8eee86e5c08ab3a686
Mohamed Ben Abbes <mohamedbenabbes6@gmail.com>
2020-07-22 06:46:01 +0100

    Update plotting_with_matplotlib.py

--- plotting_with_matplotlib.py
+++ plotting_with_matplotlib.py
@@ -12,7 +12,7 @@ plt.scatter(x,y) # scatter plot
 plt.show()
 
 ########################################################################################
-# More plotting details                                                                      #
+# More plotting details                                                                  
 ########################################################################################
 
 # generate sample data
@@ -51,4 +51,26 @@ fig,(ax1,ax2,ax3,ax4) = plt.subplots(nrows=4, ncols=1, sharex=True, figsize=(8,4
 # Iterating the Axes within a Figure
 for ax in fig.get_axes():
     pass # do something
+
+########################################################################################
+# ax_plot() example                                                                 
+########################################################################################
+
+# generate sample data
+x = np.linspace(0, 20, 1000)
+y = np.sin(x)
+
+fig = plt.figure(figsize=(8,4)) # get an empty figure and add an Axes
+ax = fig.add_subplot(1,1,1) # row-col-num
+
+ax.plot(x, y, 'b-', linewidth=2, label='Sample label') # line plot data on the Axes
+
+# add title, labels and legend, etc.
+ax.set_ylabel('y axis label', fontsize=16) # y label
+ax.set_xlabel('x axis label', fontsize=16) # x label
+ax.legend(loc='best') # legend
+ax.grid(True) # show grid
+fig.suptitle('Sample Plot Title') # title
+fig.tight_layout(pad=1) # tidy laytout
+fig.savefig('filename.png', dpi=125)
 
